5ee5d910c8c1d4f66bb400971006753a68783c26
alexandrea parks <alparks@idk.com>
2024-04-01 17:37:24 -0500

    completed lecture videos 1-3

---
+++ would-you-rather/src/App.vue
@@ -0,0 +1,36 @@
+<script setup>
+// PARENT
+
+import WouldYouRather from './components/WouldYouRather.vue'
+
+import { ref } from 'vue'
+
+// this data will be passed to the Child component
+const wyrQuestion = ref('Dance or Sing?')
+const wyrAnswer1 = ref('Dance')
+const wyrAnswer2 = ref('Sing')
+
+// store user's answer
+const userSelection = ref('')
+
+
+
+</script>
+
+<template>
+
+  <h1>Would You Rather...</h1>
+
+  <!--link data in Parent component to Child props-->
+  <!--the Child prop 'question' binds to the Parent 'wyrQuestion'-->
+  <WouldYouRather
+      v-bind:question="wyrQuestion"
+      v-bind:answer1="wyrAnswer1"
+      v-bind:answer2="wyrAnswer2"
+  ></WouldYouRather>
+
+</template>
+
+<style scoped>
+
+</style>
---
+++ would-you-rather/src/components/WouldYouRather.vue
@@ -0,0 +1,42 @@
+<script setup>
+// CHILD
+
+import { ref } from 'vue'
+
+
+// Child stores data passed from Parent in props
+defineProps({
+  question: String,
+  answer1: String,
+  answer2: String
+})
+
+const choice = ref('')
+
+
+</script>
+
+<template>
+
+  <h2>Make your choice!</h2>
+
+  {{ question }} <br>
+
+  <!--v-model links both radio buttons together so only one can be clicked
+  and connects them to the reactive state ref choice - when a radio button
+  is selected, the reactive value of choice changes to the selected choice-->
+
+  <!--v-bind links the radio button to it's related prop-->
+
+  <input v-model="choice" v-bind:value="answer1" type="radio" id="answer-1">
+  <label for="answer-1">{{ answer1 }}</label>
+
+  <input v-model="choice" v-bind:value="answer2" type="radio" id="answer-2">
+  <label for="answer-2">{{ answer2 }}</label>
+
+
+</template>
+
+<style scoped>
+
+</style>
