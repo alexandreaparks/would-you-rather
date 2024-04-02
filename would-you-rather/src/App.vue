b0a1ca7130ec2f58c44c0dfe998b85f3a1ddff10
alexandrea parks <alparks@idk.com>
2024-04-01 19:50:14 -0500

    completed WYR app

--- would-you-rather/src/App.vue
+++ would-you-rather/src/App.vue
@@ -13,24 +13,45 @@ const wyrAnswer2 = ref('Sing')
 // store user's answer
 const userSelection = ref('')
 
-
+function updateUserSelection(userChoice) {  // argument is choice in the Child component
+  userSelection.value = `Thanks! You chose ${userChoice}`
+}
 
 </script>
 
 <template>
 
-  <h1>Would You Rather...</h1>
+  <div id="app-component">
 
-  <!--link data in Parent component to Child props-->
-  <!--the Child prop 'question' binds to the Parent 'wyrQuestion'-->
-  <WouldYouRather
-      v-bind:question="wyrQuestion"
-      v-bind:answer1="wyrAnswer1"
-      v-bind:answer2="wyrAnswer2"
-  ></WouldYouRather>
+    <h1>Would You Rather...</h1>
+
+    <!--v-bind links data in Parent component to Child props-->
+    <!--the Child prop 'question' binds to the Parent 'wyrQuestion'-->
+
+    <!--v-on receives event from Child and calls updateUserSelection-->
+    <WouldYouRather
+        v-bind:question="wyrQuestion"
+        v-bind:answer1="wyrAnswer1"
+        v-bind:answer2="wyrAnswer2"
+
+        v-on:choice-selected="updateUserSelection"
+    ></WouldYouRather>
+
+    <p>{{ userSelection }}</p>
+
+  </div>
 
 </template>
 
 <style scoped>
+
+#app-component {
+  background-color: darkblue;
+  padding: 40px;
+}
+
+p {
+  font-family: "Courier New", Courier, monospace;
+}
 
 </style>
